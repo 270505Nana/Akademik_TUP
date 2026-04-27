af26f81f7735dc674cfec9c863f35c2926f3ac79
Nizar Daffa <izardev@gmail.com>
2026-04-27 14:08:06 +0700

    fix: remove duplicate router declaration in index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,10 @@
 var express = require("express");
-const { verifyToken } = require("../middlewares/auth");
+
+var router = express.Router();
+
 const {
   downloadTemplateUpload,
 } = require("../controllers/templateUploadController");
-var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
